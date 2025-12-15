271c2f6262b937b155209373adeafff53b50df27
ReNecro100 <jarabotaju2007@gmail.com>
2025-12-16 01:21:43 +0300

    Login page and that's it

--- FTP_interaction.py
+++ FTP_interaction.py
@@ -1,15 +1,35 @@
 from ftplib import FTP
 from os import remove
+import json
 
-def FTP_login(address):
+def FTP_connect(address):
     ftp = FTP(address)
     ftp.login()
     return ftp
 
-def FTP_get_messages(ftp_connection, dir):
-    ftp_connection.cwd(dir)
+def FTP_get_logins(ftp_connection, dir, login, password):
+    try:
+        ftp_connection.cwd(dir)
+    except:
+        pass
+    users = []
+    ftp_connection.retrlines('RETR users.json', users.append)
+    users[0] = json.loads(users[0])
+    try:
+        if users[0][login]==password:
+            print("ura")
+        else:
+            print('neljzia')
+    except:
+        print('neljzia')
+
+"""def FTP_get_messages(ftp_connection, dir):
+    try:
+        ftp_connection.cwd(dir)
+    except:
+        pass
     with open('messages.txt', 'wb') as messages:
         ftp_connection.retrbinary('RETR messages.txt', messages.write)
-    with open('messages.txt', 'r') as messages:
-        print(messages.readlines())
-    remove('messages.txt')
+    remove('messages.txt')""" #Eto potom, tochno ne v tekstovom formate
+
+#def FTP_send_message(ftp_connection, dir, message):
--- main.py
+++ main.py
@@ -7,7 +7,24 @@
 """with open("Z:/messages.txt") as file:
     print(file.readlines())"""
 # Так можно, но так не интересно
+from tkinter import *
+from tkinter import ttk
 
+import json
 from FTP_interaction import *
-ftp_connection = FTP_login('192.168.0.1')
-FTP_get_messages(ftp_connection, 'volume(sda1)')
+
+root = Tk()
+root.geometry("300x250")
+
+ftp_connection = FTP_connect('192.168.0.1')
+
+login_entry = ttk.Entry()
+login_entry.pack(anchor=NW, padx=6, pady=6)
+
+password_entry = ttk.Entry()
+password_entry.pack(anchor=NW, padx=6, pady=6)
+
+btn = ttk.Button(text="Log in", command=lambda: FTP_get_logins(ftp_connection, 'volume(sda1)/LAN_FTP_MSG_DATA', login_entry.get(), password_entry.get()))
+btn.pack(anchor=NW, padx=6, pady=6)
+
+root.mainloop()
