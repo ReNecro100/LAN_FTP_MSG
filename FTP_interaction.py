ebe04668d4da7123f01e9b0eab0157bf805fde15
ReNecro100 <jarabotaju2007@gmail.com>
2025-12-15 23:06:16 +0300

    Connects to ftp server and reads messages.txt

--- FTP_interaction.py
+++ FTP_interaction.py
@@ -1,1 +1,15 @@
-def FTP_Login
+from ftplib import FTP
+from os import remove
+
+def FTP_login(address):
+    ftp = FTP(address)
+    ftp.login()
+    return ftp
+
+def FTP_get_messages(ftp_connection, dir):
+    ftp_connection.cwd(dir)
+    with open('messages.txt', 'wb') as messages:
+        ftp_connection.retrbinary('RETR messages.txt', messages.write)
+    with open('messages.txt', 'r') as messages:
+        print(messages.readlines())
+    remove('messages.txt')
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #https://github.com/ReNecro100/LAN_FTP_MSG.git
+
+#Модульность
+#Гит
+#ФТП
+
+"""with open("Z:/messages.txt") as file:
+    print(file.readlines())"""
+# Так можно, но так не интересно
+
+from FTP_interaction import *
+ftp_connection = FTP_login('192.168.0.1')
+FTP_get_messages(ftp_connection, 'volume(sda1)')
